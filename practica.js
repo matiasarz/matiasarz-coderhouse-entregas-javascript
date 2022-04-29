05f93e316358fea6c7c368e15175f8590755a71b
MatiasArzamendia <arzamendia.matias99@gmail.com>
2022-04-28 21:16:02 -0300

    Incorporando libreria al proyecto

--- practica.js
+++
@@ -1,6 +0,0 @@
-const contenedor = document.querySelector(".container");
-
-
-contenedor.addEventListener("click",(e)=> {
-  e.target.matches(".hola") ? console.log("es el div hola") : console.log("ahora no estas en hola");
-})
